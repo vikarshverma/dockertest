19a4cf507d02bc15c681f3aabfeedebcba966340
Yen Dutt Jain <yen_jain@steris.com>
2018-04-03 14:47:41 +0530

    For Testing

--- dockertest/src/App.js
+++ dockertest/src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to DCPL</h1>
         </header>
         <p className="App-intro">
           DCPL <code>src/App.js</code> Team
